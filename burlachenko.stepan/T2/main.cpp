113504e8bff45442c7793bdcb8839c25224786d3
stepches <stepches2@gmail.com>
2026-03-31 10:55:45 +0300

    Added DataStruct struct

---
+++ burlachenko.stepan/T2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct DataStruct
+{
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+};
+
+
+
+int main()
+{
+
+
+    return 0;
+}
