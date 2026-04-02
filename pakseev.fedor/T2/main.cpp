1a6984361fb153ad92a11596ed5b830664977e68
kbnzhpp <pakseevf@mail.ru>
2026-04-03 01:19:08 +0300

    Initial commit for T2

--- pakseev.fedor/T2/main.cpp
+++ pakseev.fedor/T2/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include "DataStruct.hpp"
 
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <limits>
+
+// --- 4 ---
 int main() {
+    std::vector<DataStruct> vec;
+
+    std::copy(std::istream_iterator<DataStruct>(std::cin),
+              std::istream_iterator<DataStruct>(),
+              std::back_inserter(vec));
+
+    std::sort(vec.begin(), vec.end(), comparator);
+    std::copy(
+        std::begin(vec),
+        std::end(vec),
+        std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
     return 0;
 }
